cfcd7fc8294909a998d388690aaecbc43b07db2d
akashmadke1996 <akashmadke1996@gmail.com>
2026-08-05 20:15:48 +0530

    Ecommerce application

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -2,13 +2,12 @@ import api from './api'
 
 export const authService = {
   login(email, password) {
-  alert('login called with email: ' + email + ' and password: ' + password);
-    return api.post('/api/auth/login', { email, password })
+    return api.post('/auth/login', { email, password })
 
   },
 
   register(email, password, firstName, lastName) {
-    return api.post('/api/auth/register', {
+    return api.post('/auth/register', {
       email,
       password,
       firstName,
